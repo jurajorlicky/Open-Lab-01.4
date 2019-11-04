7b79c80f9ed0bd94baa77c11eca7f5a771f8e497
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:17:15 +0100

    Add project files.

---
+++ OpenLab 01.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OpenLab_01._4
+{
+    class Program
+    {
+        public static int spolu(int zakladna, int vyska)
+            {
+             int vysledok = (zakladna * vyska) / 2;
+             return vysledok;
+            }
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Zadaj dĺžku základňe trojuholníka :");
+        int zakladna = int.Parse(Console.ReadLine());
+        Console.WriteLine("Zadaj výšku trojuholníka :");
+        int vyska = int.Parse(Console.ReadLine());
+        int vysledok2 = spolu(zakladna, vyska);
+        Console.WriteLine($"Obsah trojuholníka je {vysledok2}");
+        Console.ReadLine();
+        }
+    }
+}
